43c50c404e8e08973ca8f22b28bc9e2e72ff579b
Mitul <mitulvpatel2211@gmail.com>
2024-04-24 19:45:25 +0530

    CorsMiddleware Registered

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -8,6 +8,7 @@ class Kernel extends HttpKernel
 {
     protected $middleware = [
         \App\Http\Middleware\TrimStrings::class,
+        \App\Http\Middleware\CorsMiddleware::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
