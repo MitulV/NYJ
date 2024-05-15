e525a3383181f41c5b75137369bf5d015fc53102
Mitul <mitulvpatel2211@gmail.com>
2024-05-15 22:29:52 +0530

    group ticket validation issue resolved

--- resources/views/admin/events/book.blade.php
+++ resources/views/admin/events/book.blade.php
@@ -138,7 +138,7 @@
                                                 onclick="decrementQuantity(this)">
                                                 -
                                             </button>
-                                            <input type="text" class="form-control text-center" value="0"
+                                            <input type="text" class="form-control text-center ticket-quantity" value="0"
                                                 name="ticket_id_{{ $ticket->id }}" readonly />
                                             <button class="btn btn-outline-secondary rounded-circle" type="button"
                                                 onclick="incrementQuantity(this, {{ $ticket->quantity }}, {{ $ticket->total_booked_tickets }})">
