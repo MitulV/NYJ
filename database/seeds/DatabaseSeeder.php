b2144f28f613ff42b4929ca09dc6734eee676a45
Mitul <mitulvpatel2211@gmail.com>
2024-05-30 00:03:38 +0530

    seeders uncommented

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
             PermissionRoleTableSeeder::class,
             UsersTableSeeder::class,
             RoleUserTableSeeder::class,
-            // CitySeeder::class,
-            // CategorySeeder::class,
+            CitySeeder::class,
+            CategorySeeder::class,
             // EventSeeder::class      
         ]);
     }
