9b8222c7c4f51eee94c85dbbe1a6b8218909dca2
Mitul <mitulvpatel2211@gmail.com>
2024-06-28 18:37:15 +0530

    Production Admin, Password changed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -5,20 +5,20 @@ use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
 {
-    public function run()
-    {
-        $users = [
-            [
-                'id'             => 1,
-                'name'           => 'Admin',
-                'email'          => 'admin@admin.com',
-                'password'       => '$2y$10$7Xn/8bPJ89ypj0cIxwoH9OOXnbK/.9xrLfFh2G4LUSRkw6j7Agn0K',
-                'remember_token' => null,
-                'created_at'     => '2019-09-24 19:16:02',
-                'updated_at'     => '2019-09-24 19:16:02',
-            ],
-        ];
+  public function run()
+  {
+    $users = [
+      [
+        'id'             => 1,
+        'name'           => 'Admin',
+        'email'          => 'nyjtickets@yahoo.com',
+        'password'       => '$2y$10$dm/PoPKtXM2s25GJeeWPduzo1GUJNjtfLrUwdQKcc4DueMNtafY9O', //Welcome@123
+        'remember_token' => null,
+        'created_at'     => '2019-09-24 19:16:02',
+        'updated_at'     => '2019-09-24 19:16:02',
+      ],
+    ];
 
-        User::insert($users);
-    }
+    User::insert($users);
+  }
 }
